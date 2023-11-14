a6bb2ad6147e77183ed5645605f500b38bb00c7c
Peng Rao <raopend@gmail.com>
2023-11-14 22:34:32 +0800

    finish week 1----Hello assignment

---
+++ src/Week1/RandomWord.java
@@ -0,0 +1,20 @@
+package Week1;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        String challenger = "";
+        String champ = "";
+        int count = 1;
+        while (!StdIn.isEmpty()) {
+            challenger = StdIn.readString();
+            if (StdRandom.bernoulli(1.0 / count)) {
+                champ = challenger;
+            }
+            count++;
+        }
+        StdOut.println(champ);
+    }
+}
